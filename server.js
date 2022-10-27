40310bd251ef0fc0f802d8a7e7e65461f30cf6fe
bhaas412 <bhaas412@gmail.com>
2022-10-26 22:44:42 -0400

    Set up server back end in server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+// Include necessary modules
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// Create port
+const PORT = process.env.PORT || 3001;
+
+// Initialize Express app
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Start server
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
